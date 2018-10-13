0367026d329180e79e7ddef1e29e6922a6ecf31e
otaree <deep.yen.ur.hash@gmail.com>
2018-10-13 08:30:09 +0530

    edited base url

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+let url;
+if (process.env.NODE_ENV  === "development") {
+  url = 'http://localhost:5000';
+} else {
+  // url = 'https://foodly-api.glitch.me/';
+  url = 'https://tranquil-falls-99160.herokuapp.com/';
+}
+
 export default axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: url
 });
